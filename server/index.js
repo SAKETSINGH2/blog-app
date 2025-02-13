b7e23cbf89d663929df70ffab6c115b1502340e8
SAKETSINGH2 <saketsingh186@gmail.com>
2025-02-13 15:12:08 +0530

    fix cors error issue

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from "cors";
 import DatabaseConnect from "./config/databaseConnection.js";
 import apiRouter from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT;
+
+app.use(
+    cors({
+        origin: "https://blog-app-pi-ivory.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 
 DatabaseConnect();
 
@@ -16,5 +24,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/backend/api", apiRouter);
 
 app.listen(PORT, () => {
-    console.log(`app is running on ${PORT}`);
+    console.log(`App is running on ${PORT}`);
 });
